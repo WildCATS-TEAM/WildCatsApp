9f367136ee7e729e9bd11aa171ff0c72cd5a22e5
BayLadyCoder <rachada_32@hotmail.com>
2020-10-16 22:32:39 -0400

    Added photos and info to each step

--- src/components/SelfExam/SelfExam.js
+++ src/components/SelfExam/SelfExam.js
@@ -1,25 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { Card, ListItem, Button, Icon } from "react-native-elements";
 import NavBar from "../NavBar";
 import { listOfQuestionsAndAnswers } from "../../lib/SelfExamResources/QuestionsAnswers";
 
-const SelfExam = ({ navigation }) => {
+const SelfExam = ({ navigation, width }) => {
   const [currentStepIndex, setCurrentStepIndex] = useState(0);
   const [onQuestionNumber, setOnQuestionNumber] = useState(1);
-  // const [answer1, setAnswer1] = useState();
-  // const [answer2, setAnswer2] = useState();
+
+  useEffect(() => {
+    if (currentStepIndex === 7) setOnQuestionNumber(3);
+  }, [currentStepIndex]);
 
   const currentStep = listOfQuestionsAndAnswers[currentStepIndex];
-
-  // const goToNextQuestion = () => setOnQuestionNumber(2);
-
   const goToFindDoctorsPage = () => navigation.navigate("Find Doctors");
   const goToSymptomInfoPage = () => navigation.navigate("Breast Cancer Info");
   const goToHomePage = () => navigation.navigate("Home");
 
-  // console.log("currentStepIndex", currentStepIndex);
-  // console.log("onQuestionNumber", onQuestionNumber);
   return (
     <View style={styles.container}>
       <NavBar onPressHome={() => navigation.navigate("Home")} />
@@ -27,15 +24,27 @@ const SelfExam = ({ navigation }) => {
       <Card style={styles.container}>
         <Card.Title>Step {currentStep.step} of 8</Card.Title>
         <Card.Divider />
-        <Card.Image source={require("../../lib/images/self-exam-1.png")} />
-        <Text style={{ marginBottom: 30, marginTop: 20 }}>
+        <Card.Image
+          source={currentStep.img}
+          style={{ height: 200, width: 200 }}
+        />
+        {currentStep.boldText && (
+          <Text style={{ marginTop: 20, fontWeight: "bold" }}>
+            {currentStep.boldText}
+          </Text>
+        )}
+        <Text style={{ marginBottom: 20, marginTop: 10 }}>
           {currentStep.description}
         </Text>
-        <Text style={{ marginBottom: 20 }}>
+        {currentStep.extra_info &&
+          currentStep.extra_info.map((info) => (
+            <Text style={{ marginBottom: 10 }}>{info}</Text>
+          ))}
+        <Text style={{ marginTop: 10, marginBottom: 20 }}>
           {currentStep[`question_${onQuestionNumber}`]}
         </Text>
-
-        {onQuestionNumber === 1 &&
+        {currentStepIndex <= 6 &&
+          onQuestionNumber === 1 &&
           currentStep.options_1.map((option, index) => (
             <Button
               key={index}
@@ -53,7 +62,8 @@ const SelfExam = ({ navigation }) => {
               title={option}
             />
           ))}
-        {onQuestionNumber === 2 &&
+        {currentStepIndex <= 6 &&
+          onQuestionNumber === 2 &&
           currentStep.options_2.map((option, index) => (
             <Button
               key={index}
@@ -67,26 +77,27 @@ const SelfExam = ({ navigation }) => {
               title={option}
             />
           ))}
-        {onQuestionNumber === 3 &&
-          currentStep.options_3.map((option, index) => (
-            <Button
-              key={index}
-              onPress={
-                index === 0
-                  ? goToFindDoctorsPage
-                  : index === 1
-                  ? goToSymptomInfoPage
-                  : goToHomePage
-              }
-              buttonStyle={{
-                borderRadius: 0,
-                marginLeft: 0,
-                marginRight: 0,
-                marginBottom: 10,
-              }}
-              title={option}
-            />
-          ))}
+        {currentStepIndex === 7 || onQuestionNumber === 3
+          ? currentStep.options_3.map((option, index) => (
+              <Button
+                key={index}
+                onPress={
+                  index === 0
+                    ? goToFindDoctorsPage
+                    : index === 1
+                    ? goToSymptomInfoPage
+                    : goToHomePage
+                }
+                buttonStyle={{
+                  borderRadius: 0,
+                  marginLeft: 0,
+                  marginRight: 0,
+                  marginBottom: 10,
+                }}
+                title={option}
+              />
+            ))
+          : null}
       </Card>
     </View>
   );
--- src/lib/SelfExamResources/QuestionsAnswers.js
+++ src/lib/SelfExamResources/QuestionsAnswers.js
@@ -1,11 +1,15 @@
 const listOfQuestionsAndAnswers = [
   {
     step: 1,
-    description: "Description for step 1",
-    question_1: "First question",
-    options_1: ["No", "Yes"],
-    question_2: "Ask symptoms question",
-    options_2: ["Symptom 1", "Symptom 2", "Symptom 3"],
+    img: require("../images/step1.jpg"),
+    boldText:
+      "Stand in front of a mirror that is large enough for you to see your breasts clearly. ",
+    description:
+      "Look for any visual changes in your breasts. Check each breast for anything unusual. Check the skin for puckering, dimpling, or scaliness. Look for a discharge from the nipples.",
+    question_1: "Did you find anything unusual?",
+    options_1: ["Yes", "No"],
+    question_2: "What did you find?",
+    options_2: ["Puckering", "Dimpling", "Scaliness"],
     question_3: "What would you like to do next?",
     options_3: [
       "Find affordable exam near me",
@@ -15,11 +19,15 @@ const listOfQuestionsAndAnswers = [
   },
   {
     step: 2,
-    description: "Description for step 2",
-    question_1: "First question",
-    options_1: ["No", "Yes"],
-    question_2: "Ask symptoms question",
-    options_2: ["Symptom 1", "Symptom 2", "Symptom 3"],
+    img: require("../images/step2.jpg"),
+
+    boldText: "Raise your arm over your head.",
+    description:
+      "Examine your right breast with your left hand. Use a skin cream with a gentle lather to make your hand glide more smoothly over the skin.",
+    question_1: "Did you find anything unusual?",
+    options_1: ["Yes", "No"],
+    question_2: "What did you find?",
+    options_2: ["Puckering", "Dimpling", "Scaliness"],
     question_3: "What would you like to do next?",
     options_3: [
       "Find affordable exam near me",
@@ -29,11 +37,19 @@ const listOfQuestionsAndAnswers = [
   },
   {
     step: 3,
-    description: "Description for step 3",
-    question_1: "First question",
-    options_1: ["No", "Yes"],
-    question_2: "Ask symptoms question",
-    options_2: ["Symptom 1", "Symptom 2", "Symptom 3"],
+    img: require("../images/step3.jpg"),
+
+    boldText: "Examining your nipples.",
+
+    description: "Use light pressure when examining the nipple.",
+    extra_info: [
+      "A change in the nipple itself may be a sign of problems. Look at it carefully to see whether there have been any changes such as inversion (nipple pulled inwards) or a discharge.",
+      "Gently squeeze the nipple a small discharge may emerge which may be: clear, dark green/brown/black, pinkish or blood stained. (If it's blood stained, see your doctor immediately)",
+    ],
+    question_1: "Did you find anything unusual?",
+    options_1: ["Yes", "No"],
+    question_2: "What did you find?",
+    options_2: ["Discharge", "Inversion", "Scaliness"],
     question_3: "What would you like to do next?",
     options_3: [
       "Find affordable exam near me",
@@ -43,11 +59,14 @@ const listOfQuestionsAndAnswers = [
   },
   {
     step: 4,
-    description: "Description for step 4",
-    question_1: "First question",
-    options_1: ["No", "Yes"],
-    question_2: "Ask symptoms question",
-    options_2: ["Symptom 1", "Symptom 2", "Symptom 3"],
+    img: require("../images/step4.jpg"),
+    boldText: "Use light pressure when examining the breast.",
+    description:
+      "Check each breast for anything unusual. Check the skin for puckering, dimpling, or scaliness.",
+    question_1: "Did you find anything unusual?",
+    options_1: ["Yes", "No"],
+    question_2: "What did you find?",
+    options_2: ["Puckering", "Dimpling", "Scaliness"],
     question_3: "What would you like to do next?",
     options_3: [
       "Find affordable exam near me",
@@ -57,11 +76,14 @@ const listOfQuestionsAndAnswers = [
   },
   {
     step: 5,
-    description: "Description for step 5",
-    question_1: "First question",
-    options_1: ["No", "Yes"],
-    question_2: "Ask symptoms question",
-    options_2: ["Symptom 1", "Symptom 2", "Symptom 3"],
+    img: require("../images/step5.jpg"),
+    boldText: "Use deep pressure when examining the breast.",
+    description:
+      "Starting at the nipple, use three finger pads to apply deep gentle pressure. Press each part of the breast in a spiral-like motion until you reach  the outer edges.",
+    question_1: "Did you find anything unusual?",
+    options_1: ["Yes", "No"],
+    question_2: "What did you find?",
+    options_2: ["Puckering", "Dimpling", "Scaliness"],
     question_3: "What would you like to do next?",
     options_3: [
       "Find affordable exam near me",
@@ -71,11 +93,14 @@ const listOfQuestionsAndAnswers = [
   },
   {
     step: 6,
-    description: "Description for step 6",
-    question_1: "First question",
-    options_1: ["No", "Yes"],
-    question_2: "Ask symptoms question",
-    options_2: ["Symptom 1", "Symptom 2", "Symptom 3"],
+    img: require("../images/step6.jpg"),
+
+    description:
+      "Check for lymph nodes under the armpit on each side and also feel above the collar bone",
+    question_1: "Did you find anything unusual?",
+    options_1: ["Yes", "No"],
+    question_2: "What did you find?",
+    options_2: ["Puckering", "Dimpling", "Scaliness"],
     question_3: "What would you like to do next?",
     options_3: [
       "Find affordable exam near me",
@@ -83,13 +108,19 @@ const listOfQuestionsAndAnswers = [
       "Set a reminder to do self exam again in one month",
     ],
   },
+
   {
     step: 7,
-    description: "Description for step 7",
-    question_1: "First question",
-    options_1: ["No", "Yes"],
-    question_2: "Ask symptoms question",
-    options_2: ["Symptom 1", "Symptom 2", "Symptom 3"],
+    img: require("../images/step7.jpg"),
+
+    boldText:
+      "Lie down in bed and stretch your arm above the bed. This allows the breast to flow over the chest wall so that the breast tissue is more easily felt.",
+    description:
+      "Use light pressure when examining the nipple. Starting at the nipple, use three finger pads to apply deep gentle pressure. Press each part of the breast in a spiral-like motion until you reach  the outer edges.",
+    question_1: "Did you find anything unusual?",
+    options_1: ["Yes", "No"],
+    question_2: "What did you find?",
+    options_2: ["Puckering", "Dimpling", "Scaliness"],
     question_3: "What would you like to do next?",
     options_3: [
       "Find affordable exam near me",
@@ -99,11 +130,14 @@ const listOfQuestionsAndAnswers = [
   },
   {
     step: 8,
-    description: "Description for step 8",
-    question_1: "First question",
-    options_1: ["No", "Yes"],
-    question_2: "Second question",
-    options_2: ["Symptom 1", "Symptom 2", "Symptom 3"],
+    img: require("../images/step8.jpg"),
+    boldText: "Repeat breast self exam in the shower.",
+    description:
+      "Your fingers will glide easily over soapy skin, so you can focus on feeling for changes underneath.",
+    question_1: "Did you find anything unusual?",
+    options_1: ["Yes", "No"],
+    question_2: "What did you find?",
+    options_2: ["Puckering", "Dimpling", "Scaliness"],
     question_3: "What would you like to do next?",
     options_3: [
       "Find affordable exam near me",
